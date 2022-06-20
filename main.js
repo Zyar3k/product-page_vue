7ef09d516e8ee70692117f0b093cb0e5a070b94d
zyar3k <a.zygan@gmail.com>
2022-06-20 12:18:52 +0200

    Render detail and variants

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const app = Vue.createApp({
       image: "./assets/images/socks_blue.jpg",
       inStock: true,
       inventory: 8,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2289, color: "blue" },
+        { id: 2290, color: "green" },
+      ],
     };
   },
 });
