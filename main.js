94bb3472fba200fc452b4325c0a7aa242300e2e7
zyar3k <a.zygan@gmail.com>
2022-06-20 12:40:30 +0200

    Add to cart and variant hover

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       image: "./assets/images/socks_blue.jpg",
       inStock: true,
       inventory: 8,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2289, color: "blue" },
-        { id: 2290, color: "green" },
+        { id: 2289, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        { id: 2290, color: "green", image: "./assets/images/socks_green.jpg" },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
